4bed39bf5bc0958ce6325fde091c5fea116a68db
yeco96 <yeisonpa.96@hotmail.com>
2019-05-04 10:42:49 -0600

    inicio 4 base

--- AllBook/allbook.sql
+++ AllBook/allbook.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 04-05-2019 a las 16:28:59
+-- Tiempo de generación: 04-05-2019 a las 18:42:26
 -- Versión del servidor: 10.1.19-MariaDB
 -- Versión de PHP: 5.6.28
 
@@ -36,6 +36,38 @@ CREATE TABLE `libro` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Tabla Libros';
 
 --
+-- Volcado de datos para la tabla `libro`
+--
+
+INSERT INTO `libro` (`id`, `codigo`, `descripcion`, `tipo`, `fechaIngreso`, `precio`) VALUES
+(1, '1500', 'JUANCHO', 'HISTORIA', '2019-05-11', '1500.000'),
+(4, '123', '123', '123', '2019-05-05', '123.000'),
+(12, '12333', '123', '123', '2019-05-04', '3333.000');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `id` varchar(45) CHARACTER SET latin1 NOT NULL,
+  `admin` tinyint(1) NOT NULL,
+  `contrasena` varchar(45) CHARACTER SET latin1 DEFAULT NULL,
+  `nombre` varchar(50) CHARACTER SET latin1 DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Tabla Usuario';
+
+--
+-- Volcado de datos para la tabla `usuario`
+--
+
+INSERT INTO `usuario` (`id`, `admin`, `contrasena`, `nombre`) VALUES
+('123', 0, '123', '123'),
+('12311', 1, '123', '333'),
+('333', 0, '3333', '333'),
+('yeco', 1, '123', 'Yeison');
+
+--
 -- Índices para tablas volcadas
 --
 
@@ -47,6 +79,12 @@ ALTER TABLE `libro`
   ADD UNIQUE KEY `codigo` (`codigo`);
 
 --
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
@@ -54,7 +92,7 @@ ALTER TABLE `libro`
 -- AUTO_INCREMENT de la tabla `libro`
 --
 ALTER TABLE `libro`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
